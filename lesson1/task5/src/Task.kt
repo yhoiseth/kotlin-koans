4328ce66001f131d56319800f612fe5a72e2118a
Yngve Høiseth <yhoiseth@gmail.com>
2017-06-10 10:21:49 +0200

    Solve task 5

--- lesson1/task5/src/Task.kt
+++ lesson1/task5/src/Task.kt
@@ -1,1 +1,12 @@
-fun containsEven(collection: Collection<Int>): Boolean = collection.any { TODO() }
+fun containsEven(collection: Collection<Int>): Boolean = collection.any {
+
+    i: Int ->
+
+    if (i % 2 == 0) {
+        true
+    } else {
+        false
+    }
+
+
+ }
